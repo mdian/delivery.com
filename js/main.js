369ad6732a70911b9095e13149ad721178490ae8
unknown <bb852852bb>
2021-09-10 01:12:00 +0300

    push until owl carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+new WOW().init();
 
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  center: true,
+  dots: false,
+  autoplay: true,
+  autoplayHoverPause: true,
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    700: {
+      items: 1,
+    },
+    992: {
+      items: 3,
+    },
+  },
+});
